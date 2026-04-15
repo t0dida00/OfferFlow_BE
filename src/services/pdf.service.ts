80eb2849c352072f29be4e0c70636fdcf7fb5cd2
t0dida00 <70305254+t0dida00@users.noreply.github.com>
2026-04-16 00:41:38 +0300

    chore: no idea

--- src/services/pdf.service.ts
+++ src/services/pdf.service.ts
@@ -1,12 +1,23 @@
-import puppeteer from 'puppeteer-core';
+import puppeteerCore from 'puppeteer-core';
+import puppeteer from 'puppeteer';
 import chromium from '@sparticuz/chromium';
+import { env } from '../config/env';
 
 export const generatePdfFromHtml = async (html: string) => {
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    executablePath: await chromium.executablePath(),
-    headless: true, // 👈 fix
-  });
+  let browser;
+
+  if (env.NODE_ENV === 'production') {
+    browser = await puppeteerCore.launch({
+      args: chromium.args,
+      executablePath: await chromium.executablePath(),
+      headless: true, // Use boolean true for production
+    });
+  } else {
+    browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      headless: true,
+    });
+  }
 
   try {
     const page = await browser.newPage();
@@ -18,6 +29,12 @@ export const generatePdfFromHtml = async (html: string) => {
     const pdfBuffer = await page.pdf({
       format: 'A4',
       printBackground: true,
+      margin: {
+        top: '20px',
+        right: '20px',
+        bottom: '20px',
+        left: '20px'
+      }
     });
 
     return pdfBuffer;
